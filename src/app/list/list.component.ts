f64d4392aab39f4a7a22c95cea052dbabeb56af1
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2019-01-12 19:28:48 +0200

    finish section 9

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StarWarsService } from '../star-wars.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
-  characters =[];
+export class ListComponent implements OnInit, OnDestroy {
+  characters = [];
   activatedRoute: ActivatedRoute;
   swService: StarWarsService;
+  loadedSide = 'all';
+  subscription;
 
   constructor(activatedRoute: ActivatedRoute, swService: StarWarsService) {
     this.activatedRoute = activatedRoute;
@@ -18,10 +21,17 @@ export class ListComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe(
-      (params) => {
-        this.characters = this.swService.getCharacters(params.side);
-      }
-    );
+    this.activatedRoute.params.subscribe(params => {
+      this.characters = this.swService.getCharacters(params.side);
+      this.loadedSide = params.side;
+    });
+    this.subscription = this.swService.charactersChanged.subscribe(_ => {
+      this.characters = this.swService.getCharacters(this.loadedSide);
+    });
+  }
+
+  ngOnDestroy() {
+    console.log('onDestroy: List');
+    this.subscription.unsubscribe();
   }
 }
--- src/app/star-wars.service.ts
+++ src/app/star-wars.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { LogService } from './log.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class StarWarsService {
@@ -9,6 +10,7 @@ export class StarWarsService {
   ];
 
   private logService: LogService;
+  charactersChanged = new Subject<void>();
 
   constructor(logService: LogService) {
     this.logService = logService;
@@ -26,20 +28,23 @@ export class StarWarsService {
       return char.name === characterInfo.name;
     });
     this.characters[pos].side = characterInfo.side;
-    this.logService.writeLog(`Updated ${characterInfo.name}'s side to ${characterInfo.side}`);
+    this.charactersChanged.next();
+    this.logService.writeLog(
+      `Updated ${characterInfo.name}'s side to ${characterInfo.side}`
+    );
   }
 
   addCharacter(name, side) {
-
     // Check if character is alerady added.
-    const pos = this.characters.findIndex( char => char.name === name );
+    const pos = this.characters.findIndex(char => char.name === name);
     if (pos !== -1) {
       return;
     }
 
-    const newCharacter = {name: name, side: side};
+    const newCharacter = { name: name, side: side };
     this.characters.push(newCharacter);
-    console.log(`Added new character: ${newCharacter.name}\nSide: ${newCharacter.side}`);
+    console.log(
+      `Added new character: ${newCharacter.name}\nSide: ${newCharacter.side}`
+    );
   }
-
 }
